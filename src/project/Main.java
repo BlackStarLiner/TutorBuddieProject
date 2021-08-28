6254f1892790e0cc69966de869cc12e91f636923
mmasivuye <mmasivuye@gmail.com>
2021-08-28 16:36:09 +0200

    Added methods to add new student to the database, added pop up boxes for confirmations and cancellations. Also added style file.

--- src/project/Main.java
+++ src/project/Main.java
@@ -14,11 +14,7 @@ import java.sql.*;
 
 public class Main extends Application {
 
-    static Connection connection = null;
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        /*();
-        if(connection != null)
-            System.out.println("Connection is successful");*/
         launch(args);
     }
 
@@ -26,28 +22,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("views/mainView.fxml"));
         primaryStage.setTitle("Tutor Buddie");
-        primaryStage.setScene(new Scene(root, 400,500));
+        primaryStage.setScene(new Scene(root, 1000,600));
         primaryStage.show();
-/*        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("TutorBuddie");
-
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("mainView.fxml"));
-        primaryLayout = loader.load();
-        Scene scene = new Scene(primaryLayout);
-        primaryStage.setScene(scene);
-        primaryStage.show();*/
-
     }
-/*
-    public static void connectToDB() throws SQLException, ClassNotFoundException {
-        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-         connection = DriverManager.getConnection("jdbc:ucanaccess://D://School//2021//WRRV301 - Project//S2//Implementation//HornetsProject1.accdb");
-         Statement statement = connection.createStatement();
-         ResultSet resultSet = statement.executeQuery("select * from Student");
-         while (resultSet.next())
-             System.out.println(resultSet.getString(2));
-         connection.close();
-    }*/
+
 
 }
--- src/project/MainController.java
+++ src/project/MainController.java
@@ -1,16 +1,18 @@
 package project;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 public class MainController {
 
@@ -24,46 +26,109 @@ public class MainController {
     public TextField cityTxtField;
     public PasswordField passTxtField;
     public PasswordField passReEnteredTxtField;
+    public Button registerBtn;
+    public Button cancelBtn;
+    public Stage mainStage;
 
 
     Connection connection = null;
-    Statement statement = null;
 
-    public MainController() throws SQLException, ClassNotFoundException {
-        connectToDB();
-    }
-
-    private void actionOnClickReqButton() throws Exception {
+    @FXML
+    private void actionOnClickReqButton() throws Exception { ;
         Parent root = FXMLLoader.load(getClass().getResource("views/registerView.fxml"));
-        Stage mainStage = (Stage)requestButton.getScene().getWindow();
+        mainStage = (Stage)requestButton.getScene().getWindow();
         mainStage.setScene(new Scene(root,600,600));
-        mainStage.show();
-
-    }
-
-
-    private void addNewStudent() throws SQLException {
-
-        DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        LocalDate now = LocalDate.now();
-
-        if(connection != null) {
-            statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("insert into Student + (fName, lName, emailAddress, startDate, streetName, " +
-                            "suburb, city, postalCode, cardNumber, cardType, cvcCode," + "expiryDate, profileImage, password)" +
-                            "values(fNameTxtField.getText(), lNameTxtField.getText(), emailTxtField.getText(),
-                            date.format(now), streetNameTxtField.getText(), suburbTxtField.getText(), cityTxtField.getText(), pCodeTxtField.getText(), null, null, null, null, null, passTxtField.getText()))"
-
-        }
     }
 
     public void connectToDB() throws SQLException, ClassNotFoundException {
+
+        System.out.println("Connecting to DB....");
         Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-        connection = DriverManager.getConnection("jdbc:ucanaccess://D://School//2021//WRRV301 - Project//S2//Implementation//HornetsProject1.accdb");
-        statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from Student");
-        while (resultSet.next())
-            System.out.println(resultSet.getString(2));
-        connection.close();
+        connection = DriverManager.getConnection("jdbc:ucanaccess://D://School//2021//WRRV301 - Project//S2//Implementation//HornetsProjectDB.accdb");
+
+        String fName = "Masivuye"; String lName = "Mpako"; String email = "mmasivuye@gmail.com";
+        String pass = "lpkpojp"; String startDate = null;
+        String streetName = "45 Loch Street"; String suburb = "North End";
+        String city = "Makhanda"; int pCode = 5060;
+
+
+        if (isConnected()) {
+
+            System.out.println("Connection to DB is found!");
+
+            Statement statement = connection.createStatement();
+
+            String sql = "insert into Student values('1', '"+fName+"', '"+lName+"', '"+pass+"', '"+email+"'," +
+                                                    " '"+startDate+"', '"+startDate+"', '"+streetName+"'," +
+                                                    "'"+suburb+"', '"+city+"', '"+pCode+"')";
+
+            statement.execute(sql);
+            System.out.println("record successfully added");
+        }
+
     }
-}
+
+    @FXML
+    public void onRegisterClicked() throws SQLException, ClassNotFoundException, IOException {
+
+        connectToDB();
+
+        String fName = fNameTxtField.getText();
+        String lName = lNameTxtField.getText();
+        String email = emailTxtField.getText();
+        LocalDate today = LocalDate.now();
+        String startDate = null;
+        String streetName = streetNameTxtField.getText();
+        String suburb = suburbTxtField.getText();
+        String city = cityTxtField.getText();
+        int pCode = Integer.parseInt(pCodeTxtField.getText());
+        String pass = passTxtField.getText();
+
+
+        if(isConnected()) {
+            System.out.println("Connection to DB is found!");
+
+            Statement statement = connection.createStatement();
+            String sql = "insert into Student values('1', '"+fName+"', '"+lName+"', '"+pass+"', '"+email+"'," +
+                    " '"+startDate+"', '"+streetName+"'," +
+                    "'"+suburb+"', '"+city+"', '"+pCode+"', '"+null+"')";
+            statement.execute(sql);
+            System.out.println("record successfully added");
+        }
+        else
+            System.out.println("lost the connection");
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Registration Information");
+        alert.setContentText("To confirm registration open your emails");
+        Optional<ButtonType> action = alert.showAndWait();
+
+        if(action.get() == ButtonType.CLOSE){
+            Parent root = FXMLLoader.load(getClass().getResource("views/mainView.fxml"));
+            mainStage = (Stage)registerBtn.getScene().getWindow();
+            mainStage.setScene(new Scene(root,1000,600));
+        }
+    }
+
+    public void onCancelClicked() throws IOException {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Cancel Confirmation");
+        alert.setHeaderText(null);
+        alert.setContentText("A you sure you want to cancel?");
+        Optional<ButtonType> action = alert.showAndWait();
+
+        if(action.get() == ButtonType.OK){
+            Parent root = FXMLLoader.load(getClass().getResource("views/mainView.fxml"));
+            mainStage = (Stage)cancelBtn.getScene().getWindow();
+            mainStage.setScene(new Scene(root,1000,600));
+        }
+
+    }
+
+    private Boolean isConnected(){
+        return connection != null;
+    }
+
+ }
+
